83a41cc9648b942fa2f5a418e22041416b4b8f46
Austin McCartney <amccartney@lanl.gov>
2017-04-10 11:34:30 -0600

    Squashed 'metaconfigure/' changes from cd179c1..edd43bb
    edd43bb local changes
    cbd8068 apple clang
    8fe650a clang compliant debug flags
    06fc1dc config
    e6fb36d more updates
    982fa8b update
    b07c58c split
    d840594 directory installation
    5a03ea0 whitespace
    4103d38 dependencies
    75d0f1d subproject linking bug
    5e9ac83 missung header extension
    2657253 ignore path default
    c486ab6 only verify version if version is available
    9507100 bug
    65038bd wrong order
    
    git-subtree-dir: metaconfigure
    git-subtree-split: edd43bb68f3376d37258d2d08a6134fe19d91189

--- description.py
+++ description.py
@@ -121,8 +121,8 @@ def evaluate_directory(state):
     for name in os.listdir(os.getcwd()):
         if os.path.isdir(os.path.join(os.getcwd(), name)):
             os.chdir(name)
-            if not re.match(name, state['ignore pattern']):
-                if re.match(name, state['test pattern']):
+            if not re.match(state['ignore pattern'], name):
+                if re.match(state['test pattern'], name):
                     evaluate_test_directory(state)
                 else:
                     evaluate_directory(state)
@@ -138,13 +138,13 @@ def collect_driver(state):
             possible_driver = 'src/main.' + extension
             if possible_driver in state['implementation files']:
                 state['driver'] = possible_driver
-                state['implementation files'].remove(driver)
+                state['implementation files'].remove(possible_driver)
                 return
         return
 
 
 def generate(name, language, **kwargs):
-    state = {'strict':True, 'test pattern':'test$', 'ignore pattern':'',
+    state = {'strict':True, 'test pattern':'test$', 'ignore pattern':'$^',
              'is external project':False}
     state.update({'name':name, 'language':language})
     state.update(kwargs)
